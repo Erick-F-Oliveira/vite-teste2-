4af552c8bfe34f3443b8ca128037ff00da6538c8
Erick Ferreira de Oliveira <63250462+Erick-F-Oliveira@users.noreply.github.com>
2024-06-05 19:14:02 +0000

    Fix bugs

---
+++ portifolio/src/components/Header/index.jsx
@@ -0,0 +1,15 @@
+import styles from "./Header.module.css";
+function Header() {
+  return (
+    <header className={styles.header}>
+      <span>Erick F Oliveira</span>
+      <nav>
+        <a href="">Home</a>
+        <a href="">Sobre</a>
+        <a href="">Projetos</a>
+        <a href="">Contatos</a>
+      </nav>
+    </header>
+  );
+}
+export default Header;
