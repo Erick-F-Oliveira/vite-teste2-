ba8a544526000465ba53d259138dd142059d9150
Erick Ferreira de Oliveira <63250462+Erick-F-Oliveira@users.noreply.github.com>
2024-06-14 15:31:21 +0000

    404 error

---
+++ portifolio/src/pages/404/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
+
+const Error404 = () => {
+  return (
+    <>
+      <Header />
+      <h1>404</h1>
+      <Footer />
+    </>
+  );
+};
+
+export default Error404;
