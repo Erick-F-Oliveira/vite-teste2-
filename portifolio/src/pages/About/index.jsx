acd8387941a18c8e414a1e4e6f8a4bb61040c594
Erick Ferreira de Oliveira <63250462+Erick-F-Oliveira@users.noreply.github.com>
2024-06-17 15:05:13 +0000

    about icons

--- portifolio/src/pages/About/index.jsx
+++ portifolio/src/pages/About/index.jsx
@@ -1,12 +1,32 @@
-
+import { DiNodejsSmall } from "react-icons/di";
+import { DiJavascript1 } from "react-icons/di";
+import { DiHtml5 } from "react-icons/di";
+import { DiCss3 } from "react-icons/di";
+import { DiMongodb } from "react-icons/di";
+import { DiReact } from "react-icons/di";
+import styles from "./About.module.css"
 
 function About() {
   return (
-    <>
-    
-        <h2>Sobre Page</h2>
+    <section className={styles.about}>    
+        <h2>Sobre mim</h2>
+        <div>
+          Foto
+        </div>
+        <div className={styles.description}>
+          Irineu<br/>
+          <p>Você num sabe nem eu</p>
+        </div>
+        <div className={styles.icons}>
+       <DiNodejsSmall className={styles.node}/>
+       <DiJavascript1/>
+       <DiHtml5/>
+       <DiCss3/>
+       <DiMongodb/>
+       <DiReact/>
+        </div>
      
-    </>
+    </section>
   );
 }
 
