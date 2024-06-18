ea9faf20b4072eaf0f464a11485c1968cf60ced4
Erick Ferreira de Oliveira <63250462+Erick-F-Oliveira@users.noreply.github.com>
2024-06-18 20:07:43 +0000

    css card and css projects

--- portifolio/src/pages/Projects/index.jsx
+++ portifolio/src/pages/Projects/index.jsx
@@ -1,9 +1,18 @@
-function Projects () {
+import Card from "../../components/Card";
+import styles from "./Projects.module.css";
+function Projects() {
   return (
-    <>
-        <h2>Projects Page</h2>    
-    </>
-  )
+    <section className={styles.projects}>
+      <h2>Meus projetos</h2><br/>
+      <div className={styles.list}>
+        <Card className={styles.card1}/>
+        <Card />
+        <Card />
+        <Card />
+        
+      </div>
+    </section>
+  );
 }
 
 export default Projects;
