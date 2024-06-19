2a4894b7b78d17eb4fbddf0f22c255c78bb96c6a
Erick Ferreira de Oliveira <63250462+Erick-F-Oliveira@users.noreply.github.com>
2024-06-19 20:00:07 +0000

    card auto fill

--- portifolio/src/pages/Projects/index.jsx
+++ portifolio/src/pages/Projects/index.jsx
@@ -1,16 +1,37 @@
+import { useEffect, useState } from "react";
 import Card from "../../components/Card";
 import styles from "./Projects.module.css";
 function Projects() {
+  const [repositories, setRepositories] = useState([]);
+  useEffect(() => {
+    const findRepo = async () => {
+      const response = await fetch(
+        "https://api.github.com/users/Erick-F-Oliveira/repos",
+      );
+      const data = await response.json();
+      setRepositories(data);
+    };
+    findRepo();
+  }, []);
+
   return (
     <section className={styles.projects}>
-      <h2>Meus projetos</h2><br/>
-      <div className={styles.list}>
-        <Card className={styles.card1}/>
-        <Card />
-        <Card />
-        <Card />
-        
-      </div>
+      <h2>Meus projetos</h2>
+      <br />
+      {repositories.length > 0 ? (
+        <section className={styles.list}>
+          {repositories.map((repo) => (
+            <Card
+              key={repo.id}
+              name={repo.name}
+              description={repo.description}
+              html_url={repo.html_url}
+            />
+          ))}
+        </section>
+      ) : (
+        <p>Carregando repositórios...</p>
+      )}
     </section>
   );
 }
